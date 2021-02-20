76645a82eda7b4fbf19df9f9e6f13dd78022dc38
Pooria Azami <pooriaazami@gmail.com>
2021-02-21 02:42:09 +0330

    Tokenizer test file generated

---
+++ src/com/pooria/utils/tests/TokenizerTest.java
@@ -0,0 +1,33 @@
+package com.pooria.utils.tests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TokenizerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void isNumber() {
+    }
+
+    @Test
+    void isParenthesis() {
+    }
+
+    @Test
+    void isOperator() {
+    }
+
+    @Test
+    void isLetter() {
+    }
+
+    @Test
+    void tokenize() {
+    }
+}
