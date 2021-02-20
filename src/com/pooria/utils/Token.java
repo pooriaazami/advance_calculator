b8acf31a82137a3e427c0c087de531a6db14973c
Pooria Azami <pooriaazami@gmail.com>
2021-02-20 20:35:17 +0330

    creating enums for Token class

--- src/com/pooria/utils/Token.java
+++ src/com/pooria/utils/Token.java
@@ -2,4 +2,15 @@ package com.pooria.utils;
 
 public class Token {
 
+    enum Priorities {
+        FUNCTION, PRODUCT, SUM, PARENTHESES
+    }
+
+    enum Type {
+        NUMBER, FUNCTION
+    }
+
+    public Token(String value, Priorities priorities, Type type) {
+
+    }
 }
