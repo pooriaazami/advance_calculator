e7a299c837d1da533d81957a7f2d46b2e2d0ae3c
Pooria Azami <pooriaazami@gmail.com>
2021-02-20 20:48:39 +0330

    implementing Comparable interface for Token class

--- src/com/pooria/utils/Token.java
+++ src/com/pooria/utils/Token.java
@@ -1,16 +1,64 @@
 package com.pooria.utils;
 
-public class Token {
+public class Token implements Comparable {
 
-    enum Priorities {
-        FUNCTION, PRODUCT, SUM, PARENTHESES
+    private final String value;
+    private final Priorities priorities;
+    private final Type type;
+
+
+    public Token(String value, Priorities priorities, Type type) {
+        this.value = value;
+        this.priorities = priorities;
+        this.type = type;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        if (!(o instanceof Token)) {
+            throw new IllegalArgumentException("You can compare token with token");
+        }
+
+        Token token = (Token) o;
+        if (token.type != this.type) {
+            throw new IllegalArgumentException("You can only compare tokens with same type");
+        }
+
+        if (this.type.getValue() < token.type.getValue())
+            return 1;
+        else if (this.type.getValue() > token.type.getValue())
+            return -1;
+
+        return 0;
+
+    }
+
+
+    public enum Priorities {
+        FUNCTION(1), PRODUCT(2), SUM(3), PARENTHESES(4);
+
+        private int value;
+
+        Priorities(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
     }
 
     enum Type {
-        NUMBER, FUNCTION
-    }
+        NUMBER(1), FUNCTION(2);
+        private int value;
 
-    public Token(String value, Priorities priorities, Type type) {
+        Type(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
 
     }
 }
